d1e20e471ae54c54e1b12bf46f90fa746e47f070
tsukasarenato <tsukasa.renato@gmail.com>
2022-08-18 21:55:14 -0300

    translating about page

--- components/about/text.tsx
+++ components/about/text.tsx
@@ -1,16 +1,18 @@
+import { useRouter } from 'next/router'
 import { aboutText } from '../../utils/about'
 
 const AboutText = () => {
   
-  const text = aboutText()
+  const { locale } = useRouter()
+  const text: string[] = aboutText(locale)
 
   return (
     <>
         <div className='flex justify-center'>
-            <h1 className='text-lg font-medium text-justify tracking-wide'>About me</h1>
+            <h1 className='text-lg font-medium text-justify tracking-wide'>{text[0]}</h1>
         </div>
         <div className='flex justify-center mt-4'>
-            <p className='text-base font-normal text-justify sm:tracking-wide'>{text}</p>
+            <p className='text-base font-normal text-justify sm:tracking-wide'>{text[1]}</p>
         </div>
     </>
   )
--- utils/about.ts
+++ utils/about.ts
@@ -1,5 +1,27 @@
-export const aboutText = () => {
-    return `Hello everyone, I'm Renato, I'm 25 years old, I'm single and live in Marília-SP, Brazil.
+export const aboutText = (language?: string) => {
+    if (language == 'pt')
+        return ['Sobre mim', `Olá a todos, sou Renato, tenho 25 anos, sou solteiro e moro em 
+        Marília-SP, Brasil. Entrei na universidade em 2015 e terminei em 2018, sou bacharel em 
+        ciência da computação pelo Centro Universitário Eurípides de Marília (UNIVEM), 
+        na universidade tive uma experiência agradável, lá publiquei alguns artigos em congresso 
+        de iniciação científica e tive um projeto apoiado pela FAPESP intitulado Interface de 
+        usuário para apoio a consciência situacional de incêndios florestais. Depois entrei 
+        no mercado de trabalho em 2019 e comecei a trabalhar com mineração de texto usando python 
+        na Universidade Estadual Paulista (UNESP), trabalhei em um projeto da UNESP apoiado pela 
+        FAPESP, depois saí de lá e entrei na empresa Neemo para trabalhar com desenvolvimento web 
+        usando Ruby on Rails, e em 2021 entrei na Select Tecnologia da Informação Eireli, 
+        atualmente trabalho aqui, aqui trabalho com vários projetos com tecnologias diferentes, 
+        como Angular, React, DotNet, Php, Python, e Django, então tive uma experiência muito 
+        diversificada na área de tecnologia e no mercado de trabalho, o que foi legal, mas eu 
+        precisava ter um trabalho mais focado em tecnologias específicas, pois o mercado de 
+        trabalho exige anos de experiência com tecnologias específicas, então para resolver 
+        esse problema escolhi um conjunto de tecnologia específica para estudar no meu tempo 
+        livre, atualmente estou focado em Python, Django e React e para demonstrar minhas 
+        habilidades com essas tecnologias criei e compartilhei meus projetos na internet. 
+        Tenho 4 projetos em andamento e você pode ver mais sobre eles no meu portfólio ou 
+        me seguindo nas redes sociais.`]
+
+    return ['About me', `Hello everyone, I'm Renato, I'm 25 years old, I'm single and live in Marília-SP, Brazil.
      I entered university in 2015 and finished in 2018, I'm a bachelor's in computing science from 
      Centro Universitário Eurípides de Marília (UNIVEM), at university I had a experience was enjoyable, 
      there I published some articles on congress of the initiation scientific and 
@@ -15,5 +37,5 @@ export const aboutText = () => {
      currently I'm focused in Python, Django and React and to demo my skills with these technologies 
      I created and shared my projects in the internet. 
      I have 4 projects in progress and you can see more about them in my portfolio or 
-     following me on social media.`
+     following me on social media.`]
 }
