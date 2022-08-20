bcdf4fbd22361337f4a2e31069ddd83142cff9cf
tsukasarenato <tsukasa.renato@gmail.com>
2022-08-20 12:51:46 -0300

    lattes icon

--- components/footer/portfolio.tsx
+++ components/footer/portfolio.tsx
@@ -1,4 +1,7 @@
+import Image from "next/image"
 import { FooterProps } from "./footer"
+import lattes from '../../public/lattes-new.svg'
+
 
  const Portfolio = ({ titles }: FooterProps) => {
     return (
@@ -9,6 +12,9 @@ import { FooterProps } from "./footer"
             <div className="flex justify-center space-x-4">
                 <a href="https://www.youtube.com/channel/UConynubYxdt4Doo60Zm-TKw" target="_blank" rel="noreferrer" className="fa fa-youtube bg-red-500 hover:bg-opacity-75 p-2 mb-2 rounded-full w-8 h-8 text-center"></a>
                 <a href="https://github.com/tsukasarenato" target="_blank" rel="noreferrer" className="fa fa-github bg-black hover:bg-opacity-75 p-2 mb-2 rounded-full w-8 h-8 text-center"></a>
+                <a href="http://lattes.cnpq.br/2058619429802273" target="_blank" rel="noreferrer" className="hover:bg-opacity-75 bg-white fill-blue-500 w-8 h-8 rounded-full text-center">
+                    <Image src={lattes} alt="lattes curriculum" className="" />
+                </a>
             </div>   
         </>
     )
