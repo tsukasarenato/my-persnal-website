94329e2c57a4e99ae2f145c3274c222c7fef571d
tsukasarenato <tsukasa.renato@gmail.com>
2022-08-25 22:01:44 -0300

    Youtube Data Collector -  post page

--- pages/post/youtube-data-collector.tsx
+++ pages/post/youtube-data-collector.tsx
@@ -1,18 +1,25 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 import Footer from '../../components/footer/footer'
 import HeadCustom from '../../components/head'
 import Menu from '../../components/menu/menu'
+import { getYoutubeDataCollectorPart1 } from '../../components/post/content/youtube-data-collector-part-1'
+import Post from '../../components/post/post'
 
 const YoutubeDataCollectorPage: NextPage = () => {
   
   const page = "/posts"
+
+  const { locale } = useRouter()
+
+  const post = getYoutubeDataCollectorPart1(locale)
 
   return (
     <>
       <HeadCustom />
       <div className="relative min-h-screen">
         <Menu page={page} />
-        <h1>Youtube Data Collector</h1>
+        <Post title={post.title} image={post.image} content={post.content} />
         <Footer />
       </div>
     </>
